a396eb94baa3b74367c8e4f20acb49067775e6aa
Furkan Demir <furkan.demir_@outlook.com>
2020-05-28 17:33:13 +0300

    update sapien.java

--- src/main/java/me/sapien/plugin/sapien/Sapien.java
+++ src/main/java/me/sapien/plugin/sapien/Sapien.java
@@ -1,23 +1,34 @@
 package me.sapien.plugin.sapien;
 
+import me.sapien.plugin.sapien.Command.Test;
+import me.sapien.plugin.sapien.Config.Config;
+import me.sapien.plugin.sapien.Core.Effect;
+import me.sapien.plugin.sapien.Engine.Runtime;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sapien extends JavaPlugin {
 
-    public static Sapien plugin;
-
+    private static Sapien instance;
     public static Sapien getInstance() {
-        return plugin;
+        return instance;
     }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        //getCommand("test").setExecutor(new Test());
 
+        Config.init();
+        Runtime.init();
+
+
+        Effect.loadAll();
+        getLogger().info("Sapien has been enabled.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Runtime.terminate();
+        getLogger().info("Sapien has been disabled.");
     }
 }
