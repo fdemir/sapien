f5d701355b3fc0a4fb9e597ee77461dac8d1a8f4
Furkan Demir <furkan.demir_@outlook.com>
2020-05-30 02:46:43 +0300

    register delay, ban and command functions

--- src/main/java/me/sapien/plugin/sapien/Core/Function.java
+++ src/main/java/me/sapien/plugin/sapien/Core/Function.java
@@ -1,13 +1,23 @@
 package me.sapien.plugin.sapien.Core;
 
+import me.sapien.plugin.sapien.Events.Command;
+import me.sapien.plugin.sapien.Functions.Ban;
 import me.sapien.plugin.sapien.Functions.Broadcast;
+import me.sapien.plugin.sapien.Functions.Delay;
 import me.sapien.plugin.sapien.Sapien;
 
 public class Function {
 
     public void loadAll() {
         // @refactor
+
         Sapien.getInstance().runtime.getRuntime().registerJavaMethod(new Broadcast(), "broadcast");
+        Sapien.getInstance().runtime.getRuntime().registerJavaMethod(new Ban(), "ban");
+        Sapien.getInstance().runtime.getRuntime().registerJavaMethod(new Delay(), "delay");
+
+        // Listener
+        Sapien.getInstance().runtime.getRuntime().registerJavaMethod(new Command(), "command");
+
     }
 
 }
