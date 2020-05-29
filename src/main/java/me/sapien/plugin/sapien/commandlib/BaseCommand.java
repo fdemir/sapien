7bfb6694ae3b95d01444070fa30547b25c7f6d55
Hasan Demirtaş <utsukushihito@outlook.com>
2020-05-30 02:21:46 +0300

    remove javadocs

--- src/main/java/me/sapien/plugin/sapien/commandlib/BaseCommand.java
+++ src/main/java/me/sapien/plugin/sapien/commandlib/BaseCommand.java
@@ -9,16 +9,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.Plugin;
 
-/**
- * Class that is the root of all sub-commands and gets registered as the command executor and tab completer
- */
 public abstract class BaseCommand extends SubCommand implements CommandExecutor, TabCompleter {
 
-    /**
-     * The name of this command, the same as in the plugin.yml. The name in the plugin.yml has to be lowercase
-     *
-     * @param name The name of this command
-     */
     public BaseCommand(final String name) {
         super(name);
     }
@@ -34,11 +26,6 @@ public abstract class BaseCommand extends SubCommand implements CommandExecutor,
         return this.onTabComplete(sender, new LinkedList<>(Arrays.asList(args)));
     }
 
-    /**
-     * Registers this command as the executor and tab completer for the given command name
-     *
-     * @param plugin The plugin to use to register this command
-     */
     public void register(final Plugin plugin) {
         // TODO Change this into reflection.
         // plugin.getCommand(this.getName()).setExecutor(this);
--- src/main/java/me/sapien/plugin/sapien/commandlib/SubCommand.java
+++ src/main/java/me/sapien/plugin/sapien/commandlib/SubCommand.java
@@ -5,9 +5,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 import org.bukkit.command.CommandSender;
 
-/**
- * Class for all sub-commands
- */
 public abstract class SubCommand {
 
     private static final String NOT_IMPLEMENTED = "This method has not been implemented yet. Sub-Command name: %name%";
@@ -16,95 +13,36 @@ public abstract class SubCommand {
 
     private final List<SubCommand> subCommands;
 
-    /**
-     * Creates a new sub-command with the provided name.
-     * <br>
-     * The name will be converted to lowercase if he isn't already
-     * <br>
-     * The name of the sub-command is what you would enter after the primary command, for example:
-     * <br>
-     * <ul>
-     *     <li><code>/command name</code></li>
-     *     <li><code>/command sub-command name</code></li>
-     * </ul>
-     *
-     * @param name The name of the command to use
-     * @throws NullPointerException If the name is null
-     */
     public SubCommand(final String name) {
         this.name = Preconditions.checkNotNull(name, "Th name for the command cannot be null!").toLowerCase();
         this.subCommands = new ArrayList<>();
     }
 
-    /**
-     * A guard that is run to check if a CommandSender is allowed to access this command
-     *
-     * @param sender The sender who issued the command or tab completion
-     * @param args The arguments that have been entered so far
-     * @param isTabCompletion <code>true</code> if this was caused by an tab completion or not
-     * @return <code>true</code> if the command sender is allowed to access this command
-     */
     public boolean guard(final CommandSender sender, final List<String> args, final boolean isTabCompletion) {
         return true;
     }
 
-    /**
-     * Adds a new sub command to this command
-     *
-     * @param command The command to add
-     * @return <code>this</code> for chained calls
-     */
     public SubCommand addSubCommand(final SubCommand command) {
         this.subCommands.add(command);
         return this;
     }
 
-    /**
-     * @return The name of this command
-     */
     public final String getName() {
         return this.name;
     }
 
-    /**
-     * Optionally you can override this method to also make this sub-command accessible via aliases.
-     * <br><br>
-     * Aliases will not show up in tab-completion
-     *
-     * @return A list of aliases for this commands name
-     */
     public List<String> getAliases() {
         return Collections.emptyList();
     }
 
-    /**
-     * This is the method that is run if no further arguments are provided to this sub command
-     *
-     * @param sender The issuer of the command
-     */
     public void run(final CommandSender sender) {
         sender.sendMessage(SubCommand.NOT_IMPLEMENTED.replace("%name%", this.getName()));
     }
 
-    /**
-     * This is the method that is run, if further arguments are provided, but are not matching any sub-command name
-     *
-     * @param sender The issuer of the command
-     * @param args Any additional arguments
-     */
     public void run(final CommandSender sender, final List<String> args) {
         sender.sendMessage(SubCommand.NOT_IMPLEMENTED.replace("%name%", this.getName()));
     }
 
-    /**
-     * This is the method that is run if this sub-command is trying to retrieve tab-completions.
-     * <br>
-     * The results of this method are merged with the names of further sub-commands of this sub-command.
-     *
-     * @param sender the issuer of the tab-completion
-     * @param args The arguments that have been provided so far, can be empty
-     * @return What the issuer should get recommended for tab-completions
-     */
     public List<String> tabComplete(final CommandSender sender, final List<String> args) {
         return Collections.emptyList();
     }
